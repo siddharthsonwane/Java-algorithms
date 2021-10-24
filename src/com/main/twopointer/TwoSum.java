fc2a2d159e1a632a146c0e2bd5d15e29f3978134
siddharth.sonwane <siddharthsonwane18@gmail.com>
2021-10-24 11:48:12 +0530

    Two Sum code added

---
+++ src/com/main/twopointer/TwoSum.java
@@ -0,0 +1,18 @@
+package com.main.twopointer;
+
+public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        if(nums.length == 0 || nums == null){
+            return new int []{};
+        }
+
+        for(int i = 0;  i<nums.length; i ++){
+            for(int j = i + 1; j<nums.length; j++){
+                if(nums[i] + nums[j] == target){
+                    return new int[]{i,j};
+                }
+            }
+        }
+        return new int[]{};
+    }
+}
